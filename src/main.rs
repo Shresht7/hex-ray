9b6eb9d4b659862cbf73ba0fd4c0dd534222f8da
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-09 23:12:57 +0530

    Read the filepath from the command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    // Collect the command-line arguments
+    let args: Vec<String> = std::env::args().collect();
+
+    // Exit if incorrect number of arguments are passed
+    if args.len() < 2 {
+        eprintln!("Usage: hex-ray <file-path>");
+        std::process::exit(1);
+    }
+
+    // Get the filepath from the arguments
+    let filepath = &args[1];
+
+    println!("{}", filepath);
 }
