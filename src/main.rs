595d8a07d437b84c004c0903fd5642297f3c9ec5
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-10 23:07:39 +0530

    Support reading in from `stdin`

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,23 @@ fn main() -> std::io::Result<()> {
     // Collect the command-line arguments
     let args: Vec<String> = std::env::args().collect();
 
-    // Exit if incorrect number of arguments are passed
-    if args.len() < 2 {
-        eprintln!("Usage: hex-ray <file-path>");
-        std::process::exit(1);
-    }
-
-    // Get the filepath from the arguments
-    let filepath = &args[1];
-
-    // Open the file
-    let mut file = std::fs::File::open(filepath)?;
-
-    // Read the file contents
+    // Buffer to store the input contents
     let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer)?;
+
+    // If no arguments are passed, read from stdin...
+    // ...otherwise, read from the filepath
+    if args.len() < 2 {
+        std::io::stdin().read_to_end(&mut buffer)?;
+    } else {
+        // Get the filepath from the arguments
+        let filepath = &args[1];
+
+        // Open the file
+        let mut file = std::fs::File::open(filepath)?;
+
+        // Read the file contents
+        file.read_to_end(&mut buffer)?;
+    }
 
     // Print the hexdump
     print::hexdump(&buffer);
