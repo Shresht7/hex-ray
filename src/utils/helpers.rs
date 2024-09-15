91477b990b68a21db72cdd28fe1b85c9afa6eb09
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-15 17:48:31 +0530

    Add docstrings

--- src/utils/helpers.rs
+++ src/utils/helpers.rs
@@ -11,6 +11,7 @@ pub fn is_printable_ascii_character(byte: &u8) -> bool {
     byte.is_ascii_graphic() || !byte.is_ascii_whitespace()
 }
 
+/// Returns a buffered reader to read from STDIN and the starting offset (Always 0 for STDIN)
 pub fn get_stdin_reader() -> Result<(Box<dyn std::io::BufRead>, usize), Box<dyn std::error::Error>>
 {
     let offset = 0; // Offset is not supported in this mode
@@ -18,11 +19,13 @@ pub fn get_stdin_reader() -> Result<(Box<dyn std::io::BufRead>, usize), Box<dyn 
     Ok((Box::new(std::io::BufReader::new(data)), offset))
 }
 
+/// Opens a file at the specified `filepath` and returns a buffered reader along with the starting offset
 pub fn get_file_reader(
     filepath: &std::path::PathBuf,
     mut offset: i64,
 ) -> Result<(Box<dyn std::io::BufRead>, usize), Box<dyn std::error::Error>> {
     let mut file = std::fs::File::open(filepath)?;
+
     // A positive offset seeks forwards from the start of the file
     if offset >= 0 {
         file.seek(std::io::SeekFrom::Start(offset as u64))?;
@@ -32,5 +35,6 @@ pub fn get_file_reader(
         file.seek(std::io::SeekFrom::End(offset))?;
         offset = file_size as i64 + offset;
     }
+
     Ok((Box::new(std::io::BufReader::new(file)), offset as usize))
 }
