bb8ec28233657ba7e4baa529f46cd7276b0a9df8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-15 18:33:47 +0530

    Setup the hello world tui example

--- src/cli/cmd/tui.rs
+++ src/cli/cmd/tui.rs
@@ -1,6 +1,13 @@
 // Library
 use clap::Parser;
 
+use ratatui::{
+    crossterm::event::{self, KeyCode, KeyEventKind},
+    style::Stylize,
+    widgets::Paragraph,
+    DefaultTerminal,
+};
+
 // -----------
 // TUI COMMAND
 // -----------
@@ -11,7 +18,28 @@ pub struct Tui {}
 
 impl Tui {
     pub fn execute(self) -> Result<(), Box<dyn std::error::Error>> {
-        println!("tui");
-        Ok(())
+        let mut terminal = ratatui::init();
+        terminal.clear()?;
+        let app_result = self.run(terminal);
+        ratatui::restore();
+        app_result
+    }
+
+    pub fn run(self, mut terminal: DefaultTerminal) -> Result<(), Box<dyn std::error::Error>> {
+        // The main draw loop
+        loop {
+            terminal.draw(|frame| {
+                let greeting = Paragraph::new("Hello Ratatui! (press 'q' to quit)")
+                    .white()
+                    .on_blue();
+                frame.render_widget(greeting, frame.area());
+            })?;
+
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    return Ok(());
+                }
+            }
+        }
     }
 }
