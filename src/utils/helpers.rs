b5b8c6e03240e96f6e0c56cc81f3eef22d836716
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-16 12:06:09 +0530

    Extract reader parsing logic into a helper function

--- src/utils/helpers.rs
+++ src/utils/helpers.rs
@@ -11,16 +11,31 @@ pub fn is_printable_ascii_character(byte: &u8) -> bool {
     byte.is_ascii_graphic() || !byte.is_ascii_whitespace()
 }
 
+// READER
+// ------
+
+/// Returns the appropriate buffered reader and the starting offset
+pub fn get_reader_and_offset(
+    filepath: Option<&std::path::PathBuf>,
+    offset: i64,
+) -> Result<(Box<dyn std::io::BufRead>, usize), Box<dyn std::error::Error>> {
+    match filepath {
+        // If a `filepath` was passed in the arguments, read the file ...
+        Some(filepath) => get_file_reader(filepath, offset),
+        // otherwise, read the input from stdin.
+        None => get_stdin_reader(),
+    }
+}
+
 /// Returns a buffered reader to read from STDIN and the starting offset (Always 0 for STDIN)
-pub fn get_stdin_reader() -> Result<(Box<dyn std::io::BufRead>, usize), Box<dyn std::error::Error>>
-{
+fn get_stdin_reader() -> Result<(Box<dyn std::io::BufRead>, usize), Box<dyn std::error::Error>> {
     let offset = 0; // Offset is not supported in this mode
     let data = std::io::stdin();
     Ok((Box::new(std::io::BufReader::new(data)), offset))
 }
 
 /// Opens a file at the specified `filepath` and returns a buffered reader along with the starting offset
-pub fn get_file_reader(
+fn get_file_reader(
     filepath: &std::path::PathBuf,
     mut offset: i64,
 ) -> Result<(Box<dyn std::io::BufRead>, usize), Box<dyn std::error::Error>> {
