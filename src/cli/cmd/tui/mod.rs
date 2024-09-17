2d825abb4559c1d0eb9439759d1d9df745b8278c
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-17 19:45:11 +0530

    Minor Refactor

--- src/cli/cmd/tui/mod.rs
+++ src/cli/cmd/tui/mod.rs
@@ -11,20 +11,25 @@ use app::App;
 
 impl View {
     pub fn execute_tui(self) -> Result<(), Box<dyn std::error::Error>> {
-        self.init();
+        self.init(); // Initialize the configuration
 
+        // Get the reader and starting offset
         let (reader, offset) = helpers::get_reader_and_offset(self.filepath.as_ref(), self.offset)?;
 
+        // Initialize the terminal
         let mut terminal = ratatui::init();
         terminal.clear()?;
 
+        // Initialize the application
         let size = terminal.size()?;
-
         let mut app = App::new(self, size.height);
         app.parse(reader, offset)?;
-        let app_result = app.run(&mut terminal);
-        ratatui::restore();
 
+        // Run the application
+        let app_result = app.run(&mut terminal);
+
+        // Restore the terminal and return
+        ratatui::restore();
         app_result
     }
 }
