36277602fd81ea631a3d33640aafc96a0916ecb9
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-09-27 16:30:17 +0530

    Update Comments

--- src/utils/format.rs
+++ src/utils/format.rs
@@ -36,31 +36,33 @@ impl From<String> for Format {
 }
 
 impl Format {
+    /// Formats the u8 value
     pub fn format(&self, data: u8) -> String {
         match &self {
-            Self::Hex => format!("{:02x}", data),
-            Self::HexWithPrefix => format!("{:#04x}", data),
-            Self::UpperHex => format!("{:02X}", data),
-            Self::UpperHexWithPrefix => format!("{:#04X}", data),
-            Self::Binary => format!("{:08b}", data),
-            Self::BinaryWithPrefix => format!("{:#010b}", data),
-            Self::Octal => format!("{:03o}", data),
-            Self::OctalWithPrefix => format!("{:#05o}", data),
-            Self::Decimal => format!("{:03}", data),
+            Self::Hex => format!("{:02x}", data),                 // e.g. 3f
+            Self::HexWithPrefix => format!("{:#04x}", data),      // e.g. 0x3f
+            Self::UpperHex => format!("{:02X}", data),            // e.g. 3F
+            Self::UpperHexWithPrefix => format!("{:#04X}", data), // e.g. 0x3F
+            Self::Binary => format!("{:08b}", data),              // e.g. 00111111
+            Self::BinaryWithPrefix => format!("{:#010b}", data),  // e.g. 0b00111111
+            Self::Octal => format!("{:03o}", data),               // e.g. 77
+            Self::OctalWithPrefix => format!("{:#05o}", data),    // e.g. 0o77
+            Self::Decimal => format!("{:03}", data),              // e.g. 63
         }
     }
 
+    /// Returns the size occupied by the format when representing the value
     pub fn size(&self) -> usize {
         match &self {
-            Format::Hex => 2,
-            Format::HexWithPrefix => 4,
-            Format::UpperHex => 2,
-            Format::UpperHexWithPrefix => 4,
-            Format::Binary => 8,
-            Format::BinaryWithPrefix => 10,
-            Format::Octal => 3,
-            Format::OctalWithPrefix => 5,
-            Format::Decimal => 3,
+            Format::Hex => 2,                // e.g. 3f
+            Format::HexWithPrefix => 4,      // e.g. 0x3f
+            Format::UpperHex => 2,           // e.g. 3F
+            Format::UpperHexWithPrefix => 4, // e.g. 0x3F
+            Format::Binary => 8,             // e.g. 00111111
+            Format::BinaryWithPrefix => 10,  // e.g. 0b00111111
+            Format::Octal => 3,              // e.g. 77
+            Format::OctalWithPrefix => 5,    // e.g. 0o77
+            Format::Decimal => 3,            // e.g. 63
         }
     }
 }
